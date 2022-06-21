e8c7814b483fb06f7de446582110a6f63a90ad12
junsj119 <wjstjddud119@naver.com>
2022-06-21 18:51:43 +0900

    ranking_Update

---
+++ TodaysHouse/src/main/java/com/hanghae/Today/sHouse/dto/PostRankingDto.java
@@ -0,0 +1,16 @@
+package com.hanghae.Today.sHouse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PostRankingDto {
+    private Long id;
+    private String imageUrl;
+    private String userNickname;
+}
--- TodaysHouse/src/main/java/com/hanghae/Today/sHouse/model/Post.java
+++ TodaysHouse/src/main/java/com/hanghae/Today/sHouse/model/Post.java
@@ -80,12 +80,12 @@ public class Post extends Timestamped {
       2-4)지금처럼
     * */
     @ManyToOne(fetch = FetchType.LAZY)
-    @JsonIgnore
+    //@JsonIgnore
     @JoinColumn(name="USER_ID")
     private User user;
 
     //@JsonManagedReference // 직렬화 허용 어노테이션
-    @JsonIgnore
+    //@JsonIgnore
     @OneToMany(mappedBy = "post", orphanRemoval = true) // orpahRemanal = true 부모 삭제시 자식도 삭제
     private List<Comment> comments;
 
