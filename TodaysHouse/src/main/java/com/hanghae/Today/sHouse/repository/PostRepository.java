b288e509a4dedd805f40d828f6b6e6bc8f895cdd
junsj119 <wjstjddud119@naver.com>
2022-06-22 22:19:15 +0900

    마이페이지 사진노출

--- TodaysHouse/src/main/java/com/hanghae/Today/sHouse/repository/PostRepository.java
+++ TodaysHouse/src/main/java/com/hanghae/Today/sHouse/repository/PostRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 import java.util.List;
@@ -19,6 +20,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("select p from Post p")
     List<Post> findAllByPostRanking(Pageable pageable);
 
-//    @Query("select p from Post p join p.user u where ")
-//    List<Post> findByMypagePicture(Pageable pageable);
+    @Query("select p from Post p join p.user u where u.id = :userId")
+    List<Post> findByMypagePicture(Pageable pageable, @Param("userId") Long userId);
 }
