815db58ecfc59636dc30abd4a7574cf316c8562b
songryel-park <fuf3413@gmail.com>
2022-06-22 10:54:51 +0900

    Merge branch 'main' of https://github.com/Today-sHouseClone/TodayHouseClone_BE into feature/sr
    # Conflicts:
    #	TodaysHouse/src/main/java/com/hanghae/Today/sHouse/service/CommentService.java

--- TodaysHouse/src/main/java/com/hanghae/Today/sHouse/repository/CommentRepository.java
+++ TodaysHouse/src/main/java/com/hanghae/Today/sHouse/repository/CommentRepository.java
@@ -2,10 +2,14 @@ package com.hanghae.Today.sHouse.repository;
 
 import com.hanghae.Today.sHouse.model.Comment;
 import com.hanghae.Today.sHouse.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
      //Comment findTopByPostIdOrderByCreatedAtDesc(Long postId);
+
+    Page<Comment> findAll(Pageable pageable);
 }
--- TodaysHouse/src/main/java/com/hanghae/Today/sHouse/service/CommentService.java
+++ TodaysHouse/src/main/java/com/hanghae/Today/sHouse/service/CommentService.java
@@ -9,6 +9,7 @@ import com.hanghae.Today.sHouse.repository.CommentRepository;
 import com.hanghae.Today.sHouse.repository.PostRepository;
 import com.hanghae.Today.sHouse.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -62,7 +63,7 @@ public class CommentService {
             LocalDateTime createdAt = comment.getCreatedAt();
             LocalDateTime modifiedAt = comment.getModifiedAt();
 
-            CommentResponseDto commentResponseDto = new CommentResponseDto(postId, id, getComment,userNickname, createdAt, modifiedAt);
+            CommentResponseDto commentResponseDto = new CommentResponseDto(postId, id, getComment, userNickname, createdAt, modifiedAt);
 
             commentResponseDtoList.add(commentResponseDto);
         }
